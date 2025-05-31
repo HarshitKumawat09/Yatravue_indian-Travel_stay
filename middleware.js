ac9e23978268e7fd0d74aee4499a10008e789acc
HarshitKumawat09 <harshitkumawat0910@gmail.com>
2025-05-31 19:11:01 +0530

    Add files via upload

--- middleware.js
+++ middleware.js
@@ -1,3 +1,9 @@
+const Listing = require("./models/listing");
+const Reviews = require("./models/review");
+const { listingSchema, reviewSchema } = require("./schema");
+const ExpressError = require("./utils/ExpressError");
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl; // Store the original URL to redirect after login
@@ -10,6 +16,26 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.saveRedirectUrl = (req, res, next) => {
     if (req.session.redirectUrl) {
         res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
+
+module.exports.isOwner = async (req, res, next) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing.owner.equals(res.locals.currUser._id)) {
+        req.flash("error", "You do not have permission to do that.");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id, reviewId } = req.params;
+    let review = await Reviews.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the author of this review.");
+        return res.redirect(`/listings/${id}`);
     }
     next();
 }
