8c81351ae5781a9f7b14ef637e65b72536c2867f
HarshitKumawat09 <harshitkumawat0910@gmail.com>
2025-06-02 18:57:16 +0530

    Add files via upload

--- models/listing.js
+++ models/listing.js
@@ -9,18 +9,22 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
+        url: String,
+        filename: String,
+    },
+//     image: {
         
-      filename: { type: String, default: "listingimage" }, // Optional filename field
-      url: {
-          type: String,
-          default:
-            "https://images.unsplash.com/photo-1445019980597-93fa8acb246c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-          set: (v) =>
-              v === ""
-                  ? "https://images.unsplash.com/photo-1445019980597-93fa8acb246c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  : v,
-      },
-  },
+//       filename: { type: String, default: "listingimage" }, // Optional filename field
+//       url: {
+//           type: String,
+//           default:
+//             "https://images.unsplash.com/photo-1445019980597-93fa8acb246c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//           set: (v) =>
+//               v === ""
+//                   ? "https://images.unsplash.com/photo-1445019980597-93fa8acb246c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+//                   : v,
+//       },
+//   },
     price: Number,
     location: String,
     country: String,
