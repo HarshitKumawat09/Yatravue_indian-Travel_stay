9d422405e3530791261987251f8d82de15d26bb8
HarshitKumawat09 <harshitkumawat0910@gmail.com>
2025-06-09 18:47:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV !== "production") {
+require('dotenv').config();
+}
+// console.log(process.env)
+
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -7,11 +13,22 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 const {listingSchema, reviewSchema} = require("./schema.js");
 const MONGO_URL = ("mongodb://127.0.0.1:27017/wanderlust");
 const Reviews = require("./models/review.js");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+const { expression } = require("joi");
+
+const itineraryRouter = require("./routes/itinerary");
+app.use("/itinerary", itineraryRouter);
+
 
 main().then(() => {
     console.log("connected to DB");
@@ -30,10 +47,47 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+    secret: "this is a secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24, // 1 day
+        maxAge: 1000 * 60 * 60 * 24 // 1 day
+    }
+};    
 
-app.get("/", (req, res) => {
-    res.send("Hi, I am root");
+
+// app.get("/", (req, res) => {
+//     res.send("Hi, I am root");
+// });
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
 });
+
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "student",
+//     });
+//     let registeredUser = await User.register(fakeUser, "Hello Namaste");
+//     res.send(registeredUser);
+// });
 
 // const validateListing = (req, res, next) => {
 //     const { error } = listingSchema.validate(req.body);
@@ -55,9 +109,9 @@ app.get("/", (req, res) => {
 //     }
 // };
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
-
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 // //Index route..
 // app.get("/listings", async (req, res) => {
 //     const allListings = await Listing.find({});
@@ -147,6 +201,14 @@ app.use("/listings/:id/reviews", reviews);
 //     console.log("sample was saved");
 //     res.send("successful testing");
 // });
+app.get('/about', (req, res) => res.render('static/about'));
+app.get('/support', (req, res) => res.render('static/support'));
+app.get('/faqs', (req, res) => res.render('static/faqs'));
+app.get('/privacy', (req, res) => res.render('static/privacy'));
+app.get('/terms', (req, res) => res.render('static/terms'));
+app.get('/why-host', (req, res) => res.render('static/why-host'));
+app.get('/host-support', (req, res) => res.render('static/host-support'));
+app.get('/host-faqs', (req, res) => res.render('static/host-faqs'));
 
 app.all("*",(req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
